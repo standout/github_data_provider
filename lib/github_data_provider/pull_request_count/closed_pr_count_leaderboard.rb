bf8aa1d107d76d775abdd0c38f4ada3d455d3a43
Emric <w.e.w@live.se>
2018-02-09 13:58:51 +0100

    Add table data to pull request count

---
+++ lib/github_data_provider/pull_request_count/closed_pr_count_leaderboard.rb
@@ -0,0 +1,30 @@
+module GithubDataProvider
+  class PullRequestCount::ClosedPRCountLeaderboard
+    # Expects the source data as a hash
+    def initialize(source_data)
+      @source_data = source_data
+    end
+
+    def parse
+      {
+        type: "table",
+        title: "Antal pull requests",
+        meta: { headers: ['Namn', 'Stängda'] },
+        data: array_data
+      }
+    end
+
+    private
+
+    def hash_data
+      @source_data["items"].each_with_object({}) do |item, hash|
+        hash[item["user"]["login"]] ||= 0
+        hash[item["user"]["login"]] += 1 if item["state"] == "closed"
+      end
+    end
+
+    def array_data
+      hash_data.map(&:to_a).sort_by {|_k, v| v }.reverse
+    end
+  end
+end
--- spec/unit/pull_request_count_spec.rb
+++ spec/unit/pull_request_count_spec.rb
@@ -3,18 +3,18 @@
 RSpec.describe GithubDataProvider::PullRequestCount do
   let(:default_response_body) do
     {
-      "total_count": 280,
-      "incomplete_results": false,
-      "items": [
+      "total_count" => 280,
+      "incomplete_results" => false,
+      "items" => [
         {
-          "user": {
-            "login": "Bob",
-            "id": 1,
-            "avatar_url": "https://secure.gravatar.com/avatar/934442aadfe3b2f4630510de416c5718?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+          "user" => {
+            "login" => "Bob",
+            "id" => 1,
+            "avatar_url" => "https://secure.gravatar.com/avatar/934442aadfe3b2f4630510de416c5718?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
           },
-          "created_at": "2009-07-12T20:10:41Z",
-          "updated_at": "2009-07-19T09:23:43Z",
-          "closed_at": nil,
+          "created_at" => "2009-07-12T20:10:41Z",
+          "updated_at" => "2009-07-19T09:23:43Z",
+          "closed_at" => nil,
         }
       ]
     }
